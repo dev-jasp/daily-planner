5a42e7363a21b6ae531d7f7945c1d2712ac85301
coyote4682 <jasphergargar@gmail.com>
2024-11-18 22:20:31 +0800

    bg color change from dark to light

--- src/Components/Column.tsx
+++ src/Components/Column.tsx
@@ -15,7 +15,11 @@ const Column: React.FC<ColumnProps> = ({ title, headingColor, column, cards, set
         <div className="w-56 shrink-0">
             <div className="mb-3 flex items-center justify-between">
                 <h3 className={`text-${headingColor}`}>{title}</h3>
+                <span className="rounded text-sm text-neutral-400">{cards.length}</span>
             </div>
+            <div
+            className= {`${ active ? "bg-neutral-800/50" : "bg-neutral-800-0"}`} 
+            ></div>
         </div>
     );
 };
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <header className='mb-20 flex items-center justify-between py-8 px-14'>
       <div className='flex flex-shrink-0 items-center'>
-        <h1 className='font-roboto text-2xl font-bold text-plain'>PeakWeek</h1>
+        <h1 className='font-roboto text-2xl font-bold text-dark'>PeakWeek</h1>
       </div>
       <div><Switch defaultChecked onChange={onChange} className='flex items-end'/></div>
     </header>
