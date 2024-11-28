735014c3206525e32f0fd8e31ef41feea10cf93a
coyote4682 <jasphergargar@gmail.com>
2024-11-28 21:12:58 +0800

    cards component added

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -5,18 +5,21 @@ function Board() {
     const [cards, setCards] = useState<any[]>([]); 
 
     return (
-        <div className="flex flex-col gap-8 h-screen w-full px-14 py-12">
+        <div className="flex flex-col lg:flex-row gap-8 h-screen w-full px-14 py-12">
             <div className="flex gap-3 mb-40">
                 <Column 
                 title="Monday" 
                 headingColor="blue-500 font-bold" 
+                column=""
                 cards={cards} 
                 setCards={setCards} 
+                
                 />
             
                 <Column 
                 title="Tuesday" 
                 headingColor="blue-500 font-bold" 
+                column=""
                 cards={cards} 
                 setCards={setCards} 
                 />
@@ -25,12 +28,14 @@ function Board() {
                 <Column 
                 title="Wednesday" 
                 headingColor="blue-500 font-bold" 
+                column=""
                 cards={cards} 
                 setCards={setCards} 
                 />
                 <Column 
                 title="Thursday" 
                 headingColor="blue-500 font-bold" 
+                column=""
                 cards={cards} 
                 setCards={setCards} 
                 />
@@ -39,20 +44,23 @@ function Board() {
                 <Column 
                 title="Friday" 
                 headingColor="blue-500 font-bold" 
+                column=""
                 cards={cards} 
                 setCards={setCards} 
                 />
                 <Column 
                 title="Saturday" 
                 headingColor="blue-500 font-bold" 
+                column=""
                 cards={cards} 
                 setCards={setCards} 
                 />
             </div>
-            <div className="flex gap-3 mb-40">
+            <div className="flex gap-3 pb-40">
                 <Column 
                 title="Sunday" 
                 headingColor="blue-500 font-bold" 
+                column=""
                 cards={cards} 
                 setCards={setCards} 
                 />
@@ -60,5 +68,34 @@ function Board() {
         </div>
     );
 }
+const PRESET_CARDS = [
+    // BACKLOG
+    { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
+    { title: "SOX compliance checklist", id: "2", column: "backlog" },
+    { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
+    { title: "Document Notifications service", id: "4", column: "backlog" },
+    // TODO
+    {
+      title: "Research DB options for new microservice",
+      id: "5",
+      column: "todo",
+    },
+    { title: "Postmortem for outage", id: "6", column: "todo" },
+    { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+  
+    // DOING
+    {
+      title: "Refactor context providers to use Zustand",
+      id: "8",
+      column: "doing",
+    },
+    { title: "Add logging to daily CRON", id: "9", column: "doing" },
+    // DONE
+    {
+      title: "Set up DD dashboards for Lambda listener",
+      id: "10",
+      column: "done",
+    },
+  ];
 
 export default Board;
