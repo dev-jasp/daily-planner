2bf59b1be4f9c05a8c76d5d33bcddbc0e7ef4b19
coyote4682 <jasphergargar@gmail.com>
2025-01-20 11:13:56 +0800

    import fix

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header: React.FC = () => (
   <div className="border-b border-neutral-800 p-4">
-    <h1 className="text-2xl font-bold text-center text-sky-600">
+    <h1 className="text-2xl font-bold text-center text-amber-500">
       Peak Week
     </h1>
   </div>
