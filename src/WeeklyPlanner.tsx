9db0632f1cbf57baade0db5b386c7616aaefcd96
coyote4682 <jasphergargar@gmail.com>
2025-01-20 11:53:48 +0800

    fix

--- src/WeeklyPlanner.tsx
+++ src/WeeklyPlanner.tsx
@@ -1,4 +1,4 @@
-import { Header } from './components/Header'; 
+import { Header } from './components/Header';
 import { PlannerBoard } from './components/PlannerBoard'; 
 
 const WeeklyPlanner: React.FC = () => {
